d4f184b114f9dcaf855b3c23b4ec4feb1fdecef5
zara-barbaric <zb64074@student.uni-lj.si>
2026-09-13 15:04:49 +0200

    change

--- code/pt_electron/mg5_cqe_c.sh
+++ code/pt_electron/mg5_cqe_c.sh
@@ -2,15 +2,17 @@
 wils="qe"
 index=727
 
+mkdir -p "/scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/pt"
+
 for pt in $(seq 20 5 500); do
-	if [[ -d "/scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/meja/pt_${pt}" ]]; then
-		rm -r /scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/meja/pt_${pt}
+	if [[ -d "/scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/pt/pt_${pt}" ]]; then
+		rm -r "/scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/pt/pt_${pt}"
 	fi
 /home/barbariczara/python3/bin/python3.7 /home/barbariczara/tools/mg5_amc/bin/mg5_aMC <<EOF
 import model SMEFTsim_general_MwScheme_UFO
 define p = g u d s u~ d~ s~
 generate p p > c e+ e- NP==1 / h a z h1 z1 c~
-output /scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/meja/pt_${pt}     
+output /scratch/barbariczara/2026/mg5/C_${wils}/pp_cee/pt/pt_${pt}     
 launch
 0
 set param_card smeft ${index} 0.0423928
--- code/wils_electron/mg5_cqe_c~.sh
+++ code/wils_electron/mg5_cqe_c~.sh
@@ -2,19 +2,21 @@
 wils="qe"
 index=727
 
+mkdir -p "/scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/wils"
+
 for C in $(seq 0 0.5 30); do
 	if [ "$C" = "0.0" ]; then
         	C=0.01
         fi
-        if [[ -d "/scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/C_${C}" ]]; then
-		rm -r /scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/C_${C}
+        if [[ -d "/scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/wils/C_${C}" ]]; then
+		rm -r "/scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/wils/C_${C}"
 	fi
-        
+	
 /home/barbariczara/python3/bin/python3.7 /home/barbariczara/tools/mg5_amc/bin/mg5_aMC <<EOF
 import model SMEFTsim_general_MwScheme_UFO
 define p = g u d s u~ d~ s~
 generate p p > c~ e+ e- NP==1 / h a z h1 z1 c
-output /scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/C_${C}     
+output /scratch/barbariczara/2026/mg5/C_${wils}/pp_c~ee/wils/C_${C}     
 launch
 0
 set param_card smeft ${index} ${C}         
